1359b494fe9266eb4f1e95269d06c0a0483ade39
k-jumonji <jumonji586@gmail.com>
2022-07-19 22:40:28 +0900

    Postsテーブル作成

---
+++ database/migrations/2022_09_30_112300_create_post_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('display_id')->unique()->nullable();
+            $table->bigInteger('user_id')->unsigned();
+            $table->string('image_path'); 
+            $table->string('thumb_image_path'); 
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('status')->nullable(); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
