d4a829e5a2e91fa43a7b046120947c4484d7b8f2
k-jumonji <jumonji586@gmail.com>
2022-07-24 00:20:56 +0900

    rollback時にエラーが出たため修正

--- database/migrations/2022_10_02_141534_add_theme_to_post_table.php
+++ database/migrations/2022_10_02_141534_add_theme_to_post_table.php
@@ -26,6 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('posts', function (Blueprint $table) {
+            $table->dropForeign(['theme_id']);
             $table->dropColumn('theme_id');
         });
     }
