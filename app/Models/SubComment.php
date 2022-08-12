46ed7835a5bc358c1db9c63e390ca9ac8fbc094e
k-jumonji <jumonji586@gmail.com>
2022-08-12 23:52:32 +0900

    コメント返信の時刻設定も設定

--- app/Models/SubComment.php
+++ app/Models/SubComment.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use DateTimeInterface;
 
 class SubComment extends Model
 {
@@ -12,5 +13,9 @@ class SubComment extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format('Y/m/d H:i');
     }
 }
