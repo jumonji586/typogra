368b1974e5ac45cbe3ae7b73a8709689e3227c26
k-jumonji <jumonji586@gmail.com>
2022-10-15 19:58:18 +0900

    トップロゴ変更

--- resources/views/common-parts/header-logo.blade.php
+++ resources/views/common-parts/header-logo.blade.php
@@ -1,6 +1,6 @@
 <h1>
     <a href="/" class="header-logo">
-        <img class="header-logo__img" src="/img/logo.png" alt="TYPOGRA/タイポグラ">
-        <img class="header-logo__img--2" src="/img/logo.png" alt="TYPOGRA/タイポグラ">
+        <img class="header-logo__img sharp" src="/img/logo.png" alt="TYPOGRA タイポグラ 作字 TYPOGRAPHY タイポグラフィ">
+        <img class="header-logo__img--2 sharp" src="/img/logo.png" alt="TYPOGRA タイポグラ 作字 TYPOGRAPHY タイポグラフィ">
     </a>
 </h1>
